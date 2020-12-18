be721a0c3f02a902ee1e3d6fb65af87c0b4642a8
pavol <paul.collins@centrum.sk>
2020-12-18 09:27:35 +0100

    add text

--- src/test/java/pajo/nothing.java
+++ src/test/java/pajo/nothing.java
@@ -1,17 +1,5 @@
 package pajo;
 
 public class nothing {
-    /// comment
-
-    //comment2
-
-    //comment3
-
-    // new branch - ducu-uataa
-
-    //comment v master
-
-    //Palo Branch
-
-//    mmm
+//toto je master
 }
